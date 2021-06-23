692faa93e9010a4f2506621f3f970db570efc4c6
Easy <easychen@qq.com>
2021-06-23 11:51:51 +0800

    新增带access token缓存的实现。

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php
-
+// config
+// ======================================
 define('SENDKEY', 'set_a_sendkey');
 define('WECOM_CID', '企业微信公司ID');
 define('WECOM_SECRET', '企业微信应用Secret');
 define('WECOM_AID', '企业微信应用ID');
 define('WECOM_TOUID', '@all');
+
+// 以下配置需要有 redis 服务和 phpredis 扩展
+define('REDIS_ON', false);
+define('REDIS_HOST', '127.0.0.1');
+define('REDIS_PORT', '6379');
+define('REDIS_EXPIRED', '7000');
+define('REDIS_KEY', 'wecom_access_token');
+
+// code
+// ======================================
 
 if (strlen(@$_REQUEST['sendkey'])  < 1
     || strlen(@$_REQUEST['text'])  < 1 || @$_REQUEST['sendkey'] != SENDKEY
@@ -16,12 +27,33 @@ header("Content-Type: application/json; charset=UTF-8");
 echo send_to_wecom(@$_REQUEST['text'], WECOM_CID, WECOM_SECRET, WECOM_AID, WECOM_TOUID);
 
 
+function redis()
+{
+    if (!isset($GLOBALS['REDIS_INSTANCE']) || !$GLOBALS['REDIS_INSTANCE']) {
+        $GLOBALS['REDIS_INSTANCE'] = new Redis();
+        $GLOBALS['REDIS_INSTANCE']->connect(REDIS_HOST, REDIS_PORT);
+    }
+
+    return $GLOBALS['REDIS_INSTANCE'];
+}
+
 function send_to_wecom($text, $wecom_cid, $wecom_secret, $wecom_aid, $wecom_touid = '@all')
 {
-    $info = @json_decode(file_get_contents("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=".urlencode($wecom_cid)."&corpsecret=".urlencode($wecom_secret)), true);
+    $access_token = false;
+    // 如果启用redis作为缓存
+    if (REDIS_ON) {
+        $access_token = redis()->get(REDIS_KEY);
+    }
+
+    if (!$access_token) {
+        $info = @json_decode(file_get_contents("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=".urlencode($wecom_cid)."&corpsecret=".urlencode($wecom_secret)), true);
                 
-    if ($info && isset($info['access_token']) && strlen($info['access_token']) > 0) {
-        $access_token = $info['access_token'];
+        if ($info && isset($info['access_token']) && strlen($info['access_token']) > 0) {
+            $access_token = $info['access_token'];
+        }
+    }
+    
+    if ($access_token) {
         $url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token='.urlencode($access_token);
         $data = new \stdClass();
         $data->touser = $wecom_touid;
@@ -44,7 +76,12 @@ function send_to_wecom($text, $wecom_cid, $wecom_secret, $wecom_aid, $wecom_toui
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         
         $response = curl_exec($ch);
+        if ($response !== false && REDIS_ON) {
+            redis()->set(REDIS_KEY, $access_token, REDIS_EXPIRED);
+        }
         return $response;
     }
+    
+    
     return false;
 }
